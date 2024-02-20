a48c1a794a6996d1d370ce8fe8967da21799521c
Topvennie <vincent@vallaeys.com>
2024-02-20 12:18:19 +0100

    added swagger

--- backend/ypovoli/settings.py
+++ backend/ypovoli/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',       # Swagger 
+    'rest_framework',               # Django rest framework
+    'drf_yasg'                      # Yet Another Swagger generator
 ]
 
 MIDDLEWARE = [
