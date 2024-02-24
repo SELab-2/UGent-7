13ac1e7ef13d10a6df0e23f82804f1c2ad2e9484
Bram Meir <Bram.Meir@UGent.be>
2024-02-24 22:44:34 +0100

    refactor: rename logic to api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from logic.views import logic_view
+from api.views import logic_view
 
 urlpatterns = [
     path('hello', logic_view.hello)
--- backend/ypovoli/settings.py
+++ backend/ypovoli/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
     # First party
     'authentication',               # Ypovoli authentication
-    'logic'                         # Ypovoli logic of the base application
+    'api'                         # Ypovoli logic of the base application
 ]
 
 MIDDLEWARE = [
