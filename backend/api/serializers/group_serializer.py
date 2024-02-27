974f6291c1e285e881b4fae5eb0c98ed1a956d16
Bram Meir <Bram.Meir@UGent.be>
2024-02-27 11:44:07 +0100

    fix: link to project instead of id

--- backend/api/serializers/group_serializer.py
+++ backend/api/serializers/group_serializer.py
@@ -3,6 +3,12 @@ from ..models.group import Group
 
 
 class GroupSerializer(serializers.ModelSerializer):
+    project = serializers.HyperlinkedRelatedField(
+      many=False,
+      read_only=True,
+      view_name='project-detail'
+    )
+
     class Meta:
         model = Group
         fields = ['id', 'project', 'students', 'score']
