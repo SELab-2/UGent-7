cbc448ad62672896b634e45ca904f780597c2116
Topvennie <vincent@vallaeys.com>
2024-02-27 15:08:24 +0100

    chore: models

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,3 +1,18 @@
+from authentication.models import User
 from django.db import models
 
-# Create your models here.
+
+class NotificationTemplate(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    title_key = models.CharField(required=True, max_length=255)
+    description_key = models.CharField(required=True, max_length=255)
+
+
+class Notification(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    user = models.ForeignKey(User, required=True, on_delete=models.CASCADE)
+    template = models.ForeignKey(NotificationTemplate, required=True, on_delete=models.CASCADE, db_column='template_id')
+    created_at = models.DateTimeField(auto_now_add=True)
+    arguments = models.JSONField(required=False, default=dict)
+    is_read = models.BooleanField(default=False)
+    is_sent = models.BooleanField(default=False)
