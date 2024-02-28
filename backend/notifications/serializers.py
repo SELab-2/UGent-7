cbc766644c07fed6a6451d994c1100d211714647
Topvennie <vincent@vallaeys.com>
2024-02-28 14:31:34 +0100

    chore: arguments error checking

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -1,3 +1,6 @@
+import re
+from typing import Dict, List
+
 from django.utils.translation import gettext as _
 from notifications.models import Notification, NotificationTemplate
 from rest_framework import serializers
@@ -25,17 +28,42 @@ class NotificationSerializer(serializers.ModelSerializer):
             "is_sent",
         ]
 
+    def _get_missing_keys(self, s: str, d: Dict[str, str]) -> List[str]:
+        required_keys = re.findall(r"%\((\w+)\)", s)
+        missing_keys = [key for key in required_keys if key not in d]
+
+        return missing_keys
+
+    def validate(self, data):
+        data = super().validate(data)
+
+        if "arguments" not in data:
+            data["arguments"] = {}
+
+        title_missing = self._get_missing_keys(
+            data["template_id"].title_key, data["arguments"]
+        )
+        description_missing = self._get_missing_keys(
+            data["template_id"].description_key, data["arguments"]
+        )
+
+        if title_missing or description_missing:
+            raise serializers.ValidationError(
+                {
+                    "missing arguments": {
+                        "title": title_missing,
+                        "description": description_missing,
+                    }
+                }
+            )
+
+        return data
+
     def get_message(self, obj):
-        if obj.arguments != {}:
-            return {
-                "title": _(obj.template_id.title_key),
-                "description": _(obj.template_id.description_key) % obj.arguments,
-            }
-        else:
-            return {"title": "test", "description": "test2"}
+        return {
+            "title": _(obj.template_id.title_key),
+            "description": _(obj.template_id.description_key) % obj.arguments,
+        }
 
 
-# TODO: NotificationSerializer exclude = ['user'] and use depth = 1
-# TODO: HyperlinkedModelSerializer?
-# TODO: serializer method field as class?
-# TODO: Error checking
+# TODO: HyperlinkedModelSerializer
