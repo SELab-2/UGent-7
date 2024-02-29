4b77d4b850e1fd718dd081165e889eece38bf849
bsilkyn <brent.silkyn@ugent.be>
2024-03-01 00:19:36 +0100

    test: removing unneeded tests + eliminating all other opportunities for failure in tests #27

--- backend/authentication/tests/test_authentication.py
+++ backend/authentication/tests/test_authentication.py
@@ -1,56 +1,21 @@
+from cas_client import CASClient
 from django.test import TestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 from unittest.mock import patch
-from authentication.cas.client import client
 from authentication.serializers import CASTokenObtainSerializer, UserSerializer
 
 
-def customize_data(ugent_id, uid, mail):
+TICKET = "ST-da8e1747f248a54a5f078e3905b88a9767f11d7aedcas6"
+WRONG_TICKET = "ST-da8e1747f248a54a5f078e3905b88a9767f11d7aedcas5"
 
-    def service_validate(
-            self,
-            ticket=None,
-            service_url=None,
-            headers=None,):
-        response = {}
-        if ticket != "ST-da8e1747f248a54a5f078e3905b88a9767f11d7aedcas6":
-            response.error = "This is an error"
-        else:
-            response.data = {
-                "ugentID": ugent_id,
-                "uid": uid,
-                "mail": mail,
-                "givenname": "Dummy",
-                "surname": "McDickwad",
-                "faculty": "Sciences",
-                "lastenrolled": "2021-05-21",
-                "lastlogin": "",
-                "createtime": ""
-            }
-        return response
-
-    return service_validate
+ID = "1234"
+USERNAME = "ddickwd"
+EMAIL = "dummy@dummy.be"
+FIRST_NAME = "Dummy"
+LAST_NAME = "McDickwad"
 
 
 class UserSerializerModelTests(TestCase):
-    def test_non_string_id_makes_user_serializer_invalid(self):
-        """
-        The is_valid() method of a UserSerializer whose supplied User's ID is not a string
-        should return False.
-        """
-        user = UserSerializer(data={
-            "id": 1234
-        })
-        self.assertFalse(user.is_valid())
-
-    def test_non_string_username_makes_user_serializer_invalid(self):
-        """
-        The is_valid() method of a UserSerializer whose supplied User's username is not a string
-        should return False.
-        """
-        user = UserSerializer(data={
-            "username": 10
-        })
-        self.assertFalse(user.is_valid())
 
     def test_invalid_email_makes_user_serializer_invalid(self):
         """
@@ -58,27 +23,84 @@ class UserSerializerModelTests(TestCase):
         formatted as an email address should return False.
         """
         user = UserSerializer(data={
-            "email": "dummy"
+            'id': ID,
+            'username': USERNAME,
+            'email': 'dummy',
+            'first_name': FIRST_NAME,
+            'last_name': LAST_NAME,
         })
         user2 = UserSerializer(data={
-            "email": "dummy@dummy"
+            'id': ID,
+            'username': USERNAME,
+            'email': "dummy@dummy",
+            'first_name': FIRST_NAME,
+            'last_name': LAST_NAME,
         })
         user3 = UserSerializer(data={
-            "email": 21
+            'id': ID,
+            'username': USERNAME,
+            'email': 21,
+            'first_name': FIRST_NAME,
+            'last_name': LAST_NAME,
         })
         self.assertFalse(user.is_valid())
         self.assertFalse(user2.is_valid())
         self.assertFalse(user3.is_valid())
 
+    def test_valid_id_and_username_and_email_makes_valid_serializer(self):
+        user = UserSerializer(data={
+            'id': ID,
+            'username': USERNAME,
+            'email': EMAIL,
+            'first_name': FIRST_NAME,
+            'last_name': LAST_NAME,
+        })
+        self.assertTrue(user.is_valid())
+
+
+def customize_data(ugent_id, uid, mail):
+
+    class Response:
+        __slots__ = ('error', 'data')
+
+        def __init__(self):
+            self.error = None
+            self.data = {}
+
+    def service_validate(
+            self,
+            ticket=None,
+            service_url=None,
+            headers=None,):
+        response = Response()
+        if ticket != TICKET:
+            response.error = "This is an error"
+        else:
+            response.data['attributes'] = {
+                'ugentID': ugent_id,
+                'uid': uid,
+                'mail': mail,
+                'givenname': FIRST_NAME,
+                'surname': LAST_NAME,
+                'faculty': "Sciences",
+                'lastenrolled': "2023 - 2024",
+                'lastlogin': "",
+                'createtime': ""
+            }
+        return response
+
+    return service_validate
+
 
 class SerializersTests(TestCase):
-    @patch.object(client,
+    @patch.object(CASClient,
                   'perform_service_validate',
-                  customize_data("1234", "ddickwd", "dummy@dummy.be"))
+                  customize_data(ID, USERNAME, EMAIL))
     def test_invalid_ticket_generates_error(self):
         """When the wrong ticket is provided, a ValidationError should be raised."""
         # I have set "1" as the correct ticket hereµ
         serializer = CASTokenObtainSerializer(data={
-            'token': 'qslmdfjklmqsdfjklmqsjdkf'
+            'token': RefreshToken(),
+            'ticket': WRONG_TICKET
         })
         self.assertFalse(serializer.is_valid())
