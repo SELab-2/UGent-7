4e7d131f4372da41ed4de6823eae83a6a3ffdfae
Bram Meir <Bram.Meir@UGent.be>
2024-03-01 15:17:48 +0100

    chore: user has field notifications + faculties list

--- backend/api/serializers/teacher_serializer.py
+++ backend/api/serializers/teacher_serializer.py
@@ -9,6 +9,12 @@ class TeacherSerializer(serializers.ModelSerializer):
         read_only=True,
     )
 
+    faculties = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='faculty-detail'
+    )
+
     class Meta:
         model = Teacher
         fields = [
