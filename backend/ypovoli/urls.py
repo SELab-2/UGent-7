c27bdba2538f8e8a2e801e7d22d771e9153d079b
Bram Meir <Bram.Meir@UGent.be>
2024-03-02 18:51:59 +0100

    fix: typo

--- backend/ypovoli/urls.py
+++ backend/ypovoli/urls.py
@@ -21,7 +21,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="YpoVoli API",
+        title="Ypovoli API",
         default_version='v1',),
     public=True,
     permission_classes=(permissions.AllowAny,),
