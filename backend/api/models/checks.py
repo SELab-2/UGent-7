ecc5cb140ece5afea19b049e3f5229105bec9926
Bram Meir <Bram.Meir@UGent.be>
2024-03-06 09:36:59 +0100

    chore: refactor checks

--- backend/api/models/checks.py
+++ backend/api/models/checks.py
@@ -1,30 +1,58 @@
 from django.db import models
+from api.models.project import Project
+from api.models.extensions import ObligatedExtension, BlockedExtension
 
 
-class FileExtension(models.Model):
-    """Model that represents a file extension."""
+class StructureCheck(models.Model):
+    """Model that represents a structure check for a project.
+       This means that the structure of a submission is checked.
+       These checks are obligated to pass."""
 
-    # ID check should be generated automatically
+    # ID should be generated automatically
 
-    extension = models.CharField(max_length=10, unique=True)
-
-    def __str__(self) -> str:
-        return str(self.extension)
-
-
-class Checks(models.Model):
-    """Model that represents checks for a project."""
-
-    # ID check should be generated automatically
-
-    dockerfile = models.FileField(blank=True, null=True)
-
-    # Link to the file extensions that are allowed
-    allowed_file_extensions = models.ManyToManyField(
-        FileExtension, related_name="checks_allowed", blank=True
+    # Name of the structure check
+    name = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False
     )
 
-    # Link to the file extensions that are forbidden
-    forbidden_file_extensions = models.ManyToManyField(
-        FileExtension, related_name="checks_forbidden", blank=True
+    # Link to the project
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="structure_checks"
+    )
+
+    # Obligated extensions
+    obligated_extensions = models.ManyToManyField(
+        ObligatedExtension,
+        blank=True
+    )
+
+    # Blocked extensions
+    blocked_extensions = models.ManyToManyField(
+        BlockedExtension,
+        blank=True
+    )
+
+
+class ExtraCheck(models.Model):
+    """Model that represents an extra check for a project.
+       These checks are not obligated to pass."""
+
+    # ID should be generated automatically
+
+    # Link to the project
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="extra_checks"
+    )
+
+    # Run script
+    # TODO set upload_to
+    run_script = models.FileField(
+        blank=False,
+        null=False
     )
