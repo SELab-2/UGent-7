72a0ee2c3496baa0dd6a4718eb3cf4aee64cd88f
Bram Meir <Bram.Meir@UGent.be>
2024-03-07 21:00:35 +0100

    chore: one link for checks in project

--- backend/api/views/project_view.py
+++ backend/api/views/project_view.py
@@ -1,10 +1,13 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from django.utils.translation import gettext_lazy as _
 from api.views.check_folder_structure import get_zip_structure, check_zip_structure, data_directory, parseZipFile
 from ..models.project import Project
 from ..serializers.project_serializer import ProjectSerializer
 from ..serializers.group_serializer import GroupSerializer
+from ..serializers.checks_serializer import StructureCheckSerializer, ExtraCheckSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -27,6 +30,40 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
         except Project.DoesNotExist:
             # Invalid project ID
-            return Response(
-                status=status.HTTP_404_NOT_FOUND, data={"message": "Project not found"}
+            raise NotFound(_('project.status.not_found'))
+
+    @action(detail=True, methods=["get"])
+    def structure_checks(self, request, pk=None):
+        """Returns the structure checks for the given project"""
+
+        try:
+            queryset = Project.objects.get(id=pk)
+            checks = queryset.structure_checks.all()
+
+            # Serialize the check objects
+            serializer = StructureCheckSerializer(
+                checks, many=True, context={"request": request}
             )
+            return Response(serializer.data)
+
+        except Project.DoesNotExist:
+            # Invalid project ID
+            raise NotFound(_('project.status.not_found'))
+
+    @action(detail=True, methods=["get"])
+    def extra_checks(self, request, pk=None):
+        """Returns the extra checks for the given project"""
+
+        try:
+            queryset = Project.objects.get(id=pk)
+            checks = queryset.extra_checks.all()
+
+            # Serialize the check objects
+            serializer = ExtraCheckSerializer(
+                checks, many=True, context={"request": request}
+            )
+            return Response(serializer.data)
+
+        except Project.DoesNotExist:
+            # Invalid project ID
+            raise NotFound(_('project.status.not_found'))
