b1da49b0bd768b83254b4ed88258410237e1a4a5
Bram Meir <Bram.Meir@UGent.be>
2024-03-09 22:37:56 +0100

    fix: linting

--- backend/api/tests/test_group.py
+++ backend/api/tests/test_group.py
@@ -29,7 +29,8 @@ def create_project(name, description, days, course, group_size=2, max_score=20):
     """Create a Project with the given arguments."""
     deadline = timezone.now() + timedelta(days=days)
     return Project.objects.create(
-        name=name, description=description, deadline=deadline, course=course, group_size=group_size, max_score=max_score
+        name=name, description=description, deadline=deadline, course=course, 
+        group_size=group_size, max_score=max_score
     )
 
 
