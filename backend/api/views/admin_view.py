2300246b38f72cf2797dbc514e2227cf80433c87
EwoutV <ewoutverlinde@hotmail.com>
2024-03-10 23:01:38 +0100

    fix: add admins

--- backend/api/views/admin_view.py
+++ backend/api/views/admin_view.py
@@ -1,8 +1,8 @@
 from django.utils.translation import gettext
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-
+from rest_framework.request import Request
+from rest_framework.permissions import IsAdminUser
 from authentication.serializers import UserSerializer, UserIDSerializer
 from authentication.models import User
 
@@ -12,13 +12,14 @@ class AdminViewSet(ReadOnlyModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAdminUser]
 
-    def create(self, request):
+    def create(self, request: Request) -> Response:
         # Add an Admin
-        serializer = UserIDSerializer(data=request.data)
+        serializer = UserIDSerializer(
+            data=request.data
+        )
 
         if serializer.is_valid(raise_exception=True):
-            user = User.objects.get(serializer.validated_data["user_id"])
-            user.make_admin()
+            serializer.validated_data["user"].make_admin()
 
         return Response({
             "message": gettext("admins.success.add")
