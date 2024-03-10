b765db6487c677c4fe520a63a278803ac836ea64
Topvennie <vincent@vallaeys.com>
2024-03-10 22:56:02 +0100

    chore: generate ssl keys

--- development.sh
+++ development.sh
@@ -1,3 +1,17 @@
+echo "Checking for existing SSL certificates..."
+
+if [ ! -f "data/nginx/ssl/private.key" ] || [ ! -f "data/nginx/ssl/certificate.crt" ]; then
+echo "Generating SSL certificates..."
+    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout data/nginx/ssl/private.key \
+    -out data/nginx/ssl/certificate.crt \
+    -subj "/C=BE/ST=/L=/O=/OU=/CN="
+else
+    echo "SSL certificates already exist, skipping generation."
+fi
+
+echo "Starting services..."
 docker-compose -f development.yml up -d
 
+echo "Following logs..."
 docker-compose -f development.yml logs --follow --tail 50 backend
