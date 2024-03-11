c2024e3dfcf7467cfdfdd897b71186a3d0ccf3f5
Topvennie <vincent@vallaeys.com>
2024-03-11 16:12:47 +0100

    chore: change single user to queryset

--- backend/notifications/signals.py
+++ backend/notifications/signals.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from enum import Enum
-from typing import Dict
+from typing import Dict, List, Union
 
 from authentication.models import User
+from django.db.models.query import QuerySet
 from django.dispatch import Signal, receiver
 from django.urls import reverse
 from notifications.logic import schedule_send_mails
@@ -15,17 +16,22 @@ notification_create = Signal()
 @receiver(notification_create)
 def notification_creation(
     type: NotificationType,
-    user: User,
+    queryset: QuerySet[User],
     arguments: Dict[str, str],
     **kwargs,  # Required by django
 ) -> bool:
-    serializer = NotificationSerializer(
-        data={
-            "template_id": type.value,
-            "user": reverse("user-detail", kwargs={"pk": user.id}),
-            "arguments": arguments,
-        }
-    )
+    data: List[Dict[str, Union[str, int, Dict[str, str]]]] = []
+
+    for user in queryset:
+        data.append(
+            {
+                "template_id": type.value,
+                "user": reverse("user-detail", kwargs={"pk": user.id}),
+                "arguments": arguments,
+            }
+        )
+
+    serializer = NotificationSerializer(data=data, many=True)
 
     if not serializer.is_valid():
         return False
