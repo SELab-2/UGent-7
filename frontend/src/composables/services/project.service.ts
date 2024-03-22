03777264a24ceaa8b741b20dda95bd2243e0633e
Tybo Verslype <tybo.verslype@ugent.be>
2024-03-22 15:51:37 +0100

    chore: add project service

---
+++ frontend/src/composables/services/project.service.ts
@@ -0,0 +1,40 @@
+import {Project} from '@/types/Projects.ts';
+import {ref} from 'vue';
+import axios from 'axios';
+import {endpoints} from '@/config/endpoints.ts';
+
+export function useProject() {
+    const projects = ref<Project[]|null>(null);
+    const project = ref<Project|null>(null);
+
+    async function getProjectByID(id: number) {
+        const endpoint = endpoints.projects.retrieve.replace('{id}', id.toString());
+
+        axios.get(endpoint).then(response => {
+            project.value = Project.fromJSON(response.data);
+        }).catch(error => {
+            console.log(error.data);
+        });
+
+        console.log(project)
+    }
+
+    async function getProjectsByCourse(course_id: number) {
+        const endpoint = endpoints.projects.byCourse.replace('{course_id}', course_id.toString());
+
+        axios.get(endpoint).then(response => {
+            projects.value = response.data.map((projectData: Project) => Project.fromJSON(projectData));
+        }).catch(error => {
+            console.log(error.data);
+        });
+
+        console.log(projects.value ? projects.value.map((project, index) => `Project ${index + 1}: ${JSON.stringify(project)}`) : 'Projects is null');
+    }
+
+    return {
+        projects,
+        project,
+        getProjectByID,
+        getProjectsByCourse
+    };
+}
