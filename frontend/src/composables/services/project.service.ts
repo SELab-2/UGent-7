556172024fcd7fff2ca538e2f4dec8944b42b2f3
Tybo Verslype <tybo.verslype@ugent.be>
2024-03-22 17:46:26 +0100

    chore: add projects by date course service

--- frontend/src/composables/services/project.service.ts
+++ frontend/src/composables/services/project.service.ts
@@ -31,10 +31,33 @@ export function useProject() {
         console.log(projects.value ? projects.value.map((project, index) => `Project ${index + 1}: ${JSON.stringify(project)}`) : 'Projects is null');
     }
 
+    async function getProjectsByCourseAndDeadline(course_id: number, deadlineDate: Date ) {
+
+        const endpoint = endpoints.projects.byCourse.replace('{course_id}', course_id.toString());
+
+        axios.get(endpoint).then(response => {
+            const allProjects = response.data.map((projectData: Project) => Project.fromJSON(projectData));
+
+            // Filter projects based on the deadline date
+            const projectsWithMatchingDeadline = allProjects.filter((project: Project) => {
+                const projectDeadlineDate = project.deadline;
+                return projectDeadlineDate.toDateString() === deadlineDate.toDateString();
+            });
+
+            // Update the projects ref with the filtered projects
+            projects.value = projectsWithMatchingDeadline;
+        }).catch(error => {
+            console.log(error.data);
+        });
+
+        console.log(projects.value ? projects.value.map((project, index) => `Project ${index + 1}: ${JSON.stringify(project)}`) : 'Projects is null');
+    }
+
     return {
         projects,
         project,
         getProjectByID,
-        getProjectsByCourse
+        getProjectsByCourse,
+        getProjectsByCourseAndDeadline
     };
 }
