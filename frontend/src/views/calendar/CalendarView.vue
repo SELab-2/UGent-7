e0df9883fd191704281e9c36aea40ad2099a315e
Bram Meir <Bram.Meir@ugent.be>
2024-03-23 16:17:37 +0100

    chore: start with projects loading

--- frontend/src/views/calendar/CalendarView.vue
+++ frontend/src/views/calendar/CalendarView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import moment from 'moment';
+import 'moment/dist/locale/nl';
 import BaseLayout from '@/components/layout/BaseLayout.vue';
 import Calendar from 'primevue/calendar';
 import Title from '@/components/Title.vue';
-import moment from 'moment';
-import 'moment/dist/locale/nl';
-import { computed } from 'vue';
+import { useProject } from '@/composables/services/project.service';
+import { computed, onMounted } from 'vue';
 import {useI18n} from 'vue-i18n';
 import { ref } from 'vue';
 
@@ -14,9 +15,21 @@ const { t, locale } = useI18n();
 const selectedDate = ref(new Date());
 
 const formattedDate = computed(() => {
-    console.log(locale.value)
     // Format the selected date using moment.js
     return moment(selectedDate.value).locale(locale.value).format('DD MMMM YYYY');
+});
+
+/* Load the projects of the current student */
+const { projects, getProjectsByStudent } = useProject();
+
+// TODO: Set correct user ID
+const loadProjects = async () => {
+    await getProjectsByStudent("000210394313");
+};
+
+/* Load the projects when the component is mounted */
+onMounted(() => {
+    loadProjects();
 });
 
 </script>
@@ -36,6 +49,11 @@ const formattedDate = computed(() => {
             <div class="col-12 md:col-6">
                 <!-- Selected date on the calendar -->
                 <Title class="mb-6">{{ formattedDate }}</Title>
+
+                <!-- List of projects -->
+                <div v-for="project in projects" :key="project.id">
+                    <p>{{ project.name }}</p>
+                </div>
             </div>
         </div>
     </BaseLayout>
