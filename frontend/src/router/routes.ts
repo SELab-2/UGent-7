3e95438086911dc5bf670c9c2a844c95f7743ae3
francis <francis.vauterin@ugent.be>
2024-03-23 09:59:43 +0100

    chore: placeholder component

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,10 +1,52 @@
+import { RouteRecordRaw } from 'vue-router';
 import DashboardView from '@/views/dashboard/DashboardView.vue';
-import {RouteRecordRaw} from 'vue-router';
 import CourseView from '@/views/courses/CourseView.vue';
+import Dummy from '@/components/Dummy.vue';
 
 export const routes: RouteRecordRaw[] = [
     { path: '/', component: DashboardView, name: 'dashboard' },
     { path: '/courses', name: 'courses', children: [
         { path: ':id', component: CourseView, name: 'view' }
-    ]}
+    ]},
+    { path: '/login', component: Dummy, name: 'login' }
 ];
+
+
+/**
+ * Routes:
+ * 
+ * ### COURSES ###
+ * /courses
+ * /courses/create (teacher / admin)
+ * /courses/id
+ * /courses/id/edit
+ * /courses/id/projects
+ * /courses/id/projects/id
+ * /courses/id/projects/create
+ * /courses/id/projects/id/edit
+ * /courses/id/projects/id/groups
+ * /courses/id/projects/id/submit
+ * 
+ * ### USERS ###
+ * /users (teacher /admin)
+ * /users/students/
+ * /users/students/id
+ * /users/admins
+ * /users/admins/id
+ * /users/teachers
+ * /users/teachers/id
+ * /users/assistants
+ * /users/assistants/id
+ * /users/id (teacher / admin)
+ * 
+ * ### FACULTIES ###
+ * /faculties (teacher / admin)
+ * /faculties/create (teacher / admin)
+ * 
+ * ### NOTIFICATIONS ###
+ * /notifications
+ * /notifications/id
+ * 
+ * ### AUTHENTICATION ###
+ * /login
+ */
