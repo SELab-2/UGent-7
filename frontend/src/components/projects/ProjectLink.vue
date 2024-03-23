3abc5fa427c695c4871ebe1b0b404afa81792512
Bram Meir <bram.meir@ugent.be>
2024-03-23 21:54:48 +0100

    chore: fix project link data

--- frontend/src/components/projects/ProjectLink.vue
+++ frontend/src/components/projects/ProjectLink.vue
@@ -15,13 +15,17 @@ defineProps<{
 <template>
     <Card class="border-round">
         <template #title>
-            <h2 class="text-primary m-0 text-2xl">{{ project.course }}</h2>
+            <h2 class="text-primary m-0 text-2xl">{{ project.name }}</h2>
+        </template>
+        <template #subtitle>
+            <span class="text-sm">{{ project.course.name }}</span>
         </template>
         <template #content>
             {{ project.description }}
         </template>
         <template #footer>
-            <RouterLink :to="{ name: 'project-view', params: { id: project.id } }">
+            <!-- <RouterLink :to="{ name: 'project-view', params: { id: project.id } }"> -->
+            <RouterLink :to="{ name: 'dashboard' }">
                 <Button :icon="PrimeIcons.ARROW_RIGHT" icon-pos="right" outlined/>
             </RouterLink>
         </template>
