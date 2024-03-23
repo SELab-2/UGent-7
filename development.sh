4ddee95bedcee29ff3d63c542b9079d83925f5f0
Topvennie <vincent@vallaeys.com>
2024-03-23 16:12:19 +0100

    Revert "feat: hmr for windows"
    This reverts commit ad9e5700736b0003c989697c9b13df9e0f909d0a.

--- development.sh
+++ development.sh
@@ -2,10 +2,9 @@
 
 backend=false
 frontend=false
-windows_hmr=false
 build=false
 
-while getopts ":bfcw" opt; do
+while getopts ":bfc" opt; do
   case ${opt} in
     b )
       backend=true
@@ -13,14 +12,11 @@ while getopts ":bfcw" opt; do
     f )
       frontend=true
       ;;
-    w )
-      windows_hmr=true
-      ;;
     c )
       build=true
       ;;
     \? )
-      echo "Usage: $0 [-b] [-f] [-w] [-c]"
+      echo "Usage: $0 [-b] [-f] [-c]"
       exit 1
       ;;
   esac
@@ -36,12 +32,6 @@ if ! [ -f .env ]; then
     cp .dev.env .env
     sed -i "s/^DJANGO_SECRET_KEY=.*/DJANGO_SECRET_KEY=totally_random_key_string/" .env
     echo "Created environment file"
-fi
-
-if [ "$windows_hmr" = true ]; then
-    sed -i "s/^FRONTEND_CMD=.*/FRONTEND_CMD=\"npm run watch\"/" .env
-else
-    sed -i "s/^FRONTEND_CMD=.*/FRONTEND_CMD=\"npm run host\"/" .env
 fi
 
 echo "Checking for existing SSL certificates..."
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,10 @@
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import ViteRestart from 'vite-plugin-restart';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        ViteRestart({
-            restart: [
-              'trigger.txt',
-            ],
-          }),
-    ],
+    plugins: [vue()],
     resolve: {
         alias: {
             'font': resolve(__dirname, './src/assets/scss/theme/font'),
