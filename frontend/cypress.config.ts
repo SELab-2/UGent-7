c11b0ba331c7f6d0818a4cfa9f9aac49d7319af2
bsilkyn <brent.silkyn@ugent.be>
2024-03-24 11:34:05 +0100

    test: create test directory in src and refactor all test file to go in there #153

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "https://localhost/",
+    specPattern: "src/test/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
 });
