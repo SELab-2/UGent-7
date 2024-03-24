9a165dc5a34dc8064b9c48c41783a7c39aab9bb6
Lander Maes <landermaes@outlook.com>
2024-03-24 15:28:06 +0100

    chore: fetching projects in dashboard 2

--- frontend/src/views/dashboard/DashboardView.vue
+++ frontend/src/views/dashboard/DashboardView.vue
@@ -2,26 +2,26 @@
 import ButtonGroup from 'primevue/buttongroup';
 import Button from 'primevue/button';
 import CourseCard from '@/components/courses/CourseCard.vue';
-//import ProjectCard from '@/components/projects/ProjectCard.vue';
+import ProjectCard from '@/components/projects/ProjectCard.vue';
 import BaseLayout from '@/components/layout/BaseLayout.vue';
 import Title from '@/components/Title.vue';
 import {useI18n} from 'vue-i18n';
 import {PrimeIcons} from 'primevue/api';
 import {onMounted } from 'vue';
 import { useCourses } from '@/composables/services/courses.service.ts';
-//import { useProject } from '@/composables/services/project.service.ts';
+import { useProject } from '@/composables/services/project.service.ts';
 
 /* Composable injections */
 const { t } = useI18n();
 
 /* Service injection */
 const { courses, getCoursesByStudent } = useCourses();
-//const { projects, getProjectsByStudent } = useProject();
+const { projects, getProjectsByStudent } = useProject();
 
 onMounted(async () => {
   console.log("fetching courses");
   await getCoursesByStudent(1);  // TODO make this the id of the logged in user
-  //await getProjectsByStudent(1);
+  await getProjectsByStudent('1');
 });
 
 </script>
@@ -56,10 +56,10 @@ onMounted(async () => {
         </div>
         <!-- Project list body -->
         <div class="grid align-items-stretch">
-           <!-- <div class="col-12 md:col-6 lg:col-4 xl:col-3" v-for="project in projects">
+           <div class="col-12 md:col-6 lg:col-4 xl:col-3" v-for="project in projects">
                 <ProjectCard class="h-100" :project="project"/>
             </div>
-            -->
+
         </div>
     </BaseLayout>
 </template>
