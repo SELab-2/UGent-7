394455d1f4a7bc2169c1b26b24664998b9175917
tyboro2002 <verslypetybo@hotmail.com>
2024-03-27 15:39:27 +0100

    chore: make helpers use axios from ewout

--- frontend/src/composables/services/helpers.ts
+++ frontend/src/composables/services/helpers.ts
@@ -1,12 +1,14 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import { AxiosError, AxiosResponse } from 'axios';
+import { client } from '@/composables/axios.ts'
 import {Ref} from 'vue';
 import {ComposerTranslation} from "vue-i18n";
+import {useToastStore} from '@/store/toast.store.ts';
 const lifeTime = 3000;
 
 export async function get<T>(endpoint: string, ref: Ref<T|null>, fromJson: (data: any) => T, toast:any, t: ComposerTranslation): Promise<void> {
-    await axios.get(endpoint).then((response: AxiosResponse) => {
+    await client.get(endpoint).then((response: AxiosResponse) => {
         ref.value = fromJson(response.data);
-        //toast.add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
+        //add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
     }).catch((error: AxiosError) => {
         processError(error, toast, t);
         console.error(error); // Log the error for debugging
@@ -14,13 +16,9 @@ export async function get<T>(endpoint: string, ref: Ref<T|null>, fromJson: (data
 }
 
 export async function create<T>(endpoint: string, data:any, ref: Ref<T|null>, fromJson: (data: any) => T, toast:any,  t: ComposerTranslation): Promise<void> {
-    const headers = {
-        // TODO change this to your token
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQyODQwMjY1LCJpYXQiOjE3MTEzMDQyNjUsImp0aSI6ImQwYTgxY2YxMzU5NTQ4OWQ4OGNiZDFmZmZiMGI0MmJhIiwidXNlcl9pZCI6IjAwMDIwMTI0NzAxMSJ9.izGK0MStcMiPkOAWs0wgWsYEs0_5S1WvsleWaIcttnk"
-    };
-    await axios.post(endpoint, data, { headers }).then((response: AxiosResponse) => {
+    await client.post(endpoint, data).then((response: AxiosResponse) => {
         ref.value = fromJson(response.data);
-        //toast.add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
+        //add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
     }).catch((error: AxiosError) => {
         processError(error, toast, t);
         console.error(error); // Log the error for debugging
@@ -28,13 +26,9 @@ export async function create<T>(endpoint: string, data:any, ref: Ref<T|null>, fr
 }
 
 export async function delete_id<T>(endpoint: string, ref: Ref<T|null>, fromJson: (data: any) => T, toast:any, t: ComposerTranslation): Promise<void> {
-    const headers = {
-        // TODO change this to your token
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQyODQwMjY1LCJpYXQiOjE3MTEzMDQyNjUsImp0aSI6ImQwYTgxY2YxMzU5NTQ4OWQ4OGNiZDFmZmZiMGI0MmJhIiwidXNlcl9pZCI6IjAwMDIwMTI0NzAxMSJ9.izGK0MStcMiPkOAWs0wgWsYEs0_5S1WvsleWaIcttnk"
-    };
-    await axios.delete(endpoint,{ headers }).then((response: AxiosResponse) => {
+    await client.delete(endpoint).then((response: AxiosResponse) => {
         ref.value = fromJson(response.data);
-        //toast.add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
+        //add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
     }).catch((error: AxiosError) => {
         processError(error, toast, t);
         console.error(error); // Log the error for debugging
@@ -42,13 +36,9 @@ export async function delete_id<T>(endpoint: string, ref: Ref<T|null>, fromJson:
 }
 
 export async function delete_id_with_data<T>(endpoint: string, data: any, ref: Ref<T|null>, fromJson: (data: any) => T, toast:any, t: ComposerTranslation): Promise<void> {
-    const headers = {
-        // TODO change this to your token
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQyODQwMjY1LCJpYXQiOjE3MTEzMDQyNjUsImp0aSI6ImQwYTgxY2YxMzU5NTQ4OWQ4OGNiZDFmZmZiMGI0MmJhIiwidXNlcl9pZCI6IjAwMDIwMTI0NzAxMSJ9.izGK0MStcMiPkOAWs0wgWsYEs0_5S1WvsleWaIcttnk"
-    };
-    await axios.delete(endpoint,{ headers, data }).then((response: AxiosResponse) => {
+    await client.delete(endpoint,{ data }).then((response: AxiosResponse) => {
         ref.value = fromJson(response.data);
-        //toast.add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
+        //add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
     }).catch((error: AxiosError) => {
         processError(error, toast, t);
         console.error(error); // Log the error for debugging
@@ -57,9 +47,9 @@ export async function delete_id_with_data<T>(endpoint: string, data: any, ref: R
 
 
 export async function getList<T>(endpoint: string, ref: Ref<T[]|null>, fromJson: (data: any) => T, toast:any,  t: ComposerTranslation): Promise<void> {
-    await axios.get(endpoint).then(response => {
+    await client.get(endpoint).then(response => {
         ref.value = response.data.map((data: T) => fromJson(data));
-        //toast.add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
+        //add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
     }
     ).catch((error: AxiosError) => {
         processError(error, toast, t);
@@ -68,16 +58,15 @@ export async function getList<T>(endpoint: string, ref: Ref<T[]|null>, fromJson:
 }
 
 export async function getListMerged<T>(endpoints: string[], ref: Ref<T[]|null>, fromJson: (data: any) => T, toast:any, t: ComposerTranslation): Promise<void> {
-
     // Create an array to accumulate all response data
     const allData: T[] = [];
 
     for (const endpoint of endpoints){
         console.log(endpoint)
-        await axios.get(endpoint).then(response => {
+        await client.get(endpoint).then(response => {
             const responseData: T[] = response.data.map((data: T) => fromJson(data));
             allData.push(...responseData); // Merge into the allData array
-            // toast.add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
+            // add({severity: "success", summary: "Success Message", detail: "Order submitted", life: lifeTime});
         }
         ).catch((error: AxiosError) => {
             processError(error, toast, t);
@@ -88,20 +77,21 @@ export async function getListMerged<T>(endpoints: string[], ref: Ref<T[]|null>, 
 }
 
 export function processError(error: AxiosError, toast:any, t: ComposerTranslation){
+    const { add } = useToastStore();
     if (error.response) {
         // The request was made and the server responded with a status code
         if (error.response.status === 404) {
-            toast.add({ severity: 'error', summary: t('composables.helpers.errors.notFound'), detail: t('composables.helpers.errors.notFoundDetail'), life: lifeTime });
+            add({ severity: 'error', summary: t('composables.helpers.errors.notFound'), detail: t('composables.helpers.errors.notFoundDetail'), life: lifeTime });
         } else if (error.response.status === 401) {
-            toast.add({ severity: 'error', summary: t('composables.helpers.errors.unauthorized'), detail: t('composables.helpers.errors.unauthorizedDetail'), life: lifeTime });
+            add({ severity: 'error', summary: t('composables.helpers.errors.unauthorized'), detail: t('composables.helpers.errors.unauthorizedDetail'), life: lifeTime });
         } else {
-            toast.add({ severity: 'error', summary: t('composables.helpers.errors.server'), detail: t('composables.helpers.errors.serverDetail'), life: lifeTime });
+            add({ severity: 'error', summary: t('composables.helpers.errors.server'), detail: t('composables.helpers.errors.serverDetail'), life: lifeTime });
         }
     } else if (error.request) {
         // The request was made but no response was received
-        toast.add({ severity: 'error', summary: t('composables.helpers.errors.network'), detail: t('composables.helpers.errors.networkDetail'), life: lifeTime });
+        add({ severity: 'error', summary: t('composables.helpers.errors.network'), detail: t('composables.helpers.errors.networkDetail'), life: lifeTime });
     } else {
         // Something happened in setting up the request that triggered an error
-        toast.add({ severity: 'error', summary: t('composables.helpers.errors.request'), detail: t('composables.helpers.errors.requestDetail'), life: lifeTime });
+        add({ severity: 'error', summary: t('composables.helpers.errors.request'), detail: t('composables.helpers.errors.requestDetail'), life: lifeTime });
     }
 }
