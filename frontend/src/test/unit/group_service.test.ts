84fc5090f87f0c6843502d5c548d9a2eaf29946e
bsilkyn <brent.silkyn@ugent.be>
2024-03-28 23:44:03 +0100

    test: adjust test names

--- frontend/src/test/unit/group_service.test.ts
+++ frontend/src/test/unit/group_service.test.ts
@@ -2,8 +2,8 @@ import {describe, it, expect, beforeEach} from 'vitest'
 
 import { useGroup } from '@/composables/services/groups.service.ts'
 
-describe("admin", (): void => {
-    it("gets admin data by id", async () => {
+describe("group", (): void => {
+    it("gets group data by id", async () => {
         const response = useGroup().getGroupByID("0")
         expect(response).not.toBeNull()
     })
