1d557476d00ce2e9e237e0bb03c2c7c83f0cb651
EwoutV <ewoutverlinde@hotmail.com>
2024-03-31 19:23:35 +0200

    chore: admin panel scaffold

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -114,6 +114,11 @@ const routes: RouteRecordRaw[] = [
     // Authentication
     { path: '/auth/', children: [{ path: 'login', component: LoginView, name: 'login' }] },
 
+    // Admin
+    { path: '/admin/', children: [
+        { path: 'users', component: UsersView, name: 'admin-users' },
+    ]},
+
     // Page not found: redirect to dashboard
     { path: '/:pathMatch(.*)*', redirect: { name: 'dashboard' } },
 ];
