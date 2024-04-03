f31922f66b8e2eafcf97b8decec2cef7211c117b
EwoutV <ewoutverlinde@hotmail.com>
2024-04-03 12:50:16 +0200

    chore: course-faculty relation, faculty name translations

--- backend/api/views/faculty_view.py
+++ backend/api/views/faculty_view.py
@@ -2,10 +2,10 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 from authentication.models import Faculty
 from api.permissions.faculty_permissions import FacultyPermission
-from ..serializers.faculty_serializer import facultySerializer
+from ..serializers.faculty_serializer import FacultySerializer
 
 
 class FacultyViewSet(viewsets.ModelViewSet):
     queryset = Faculty.objects.all()
-    serializer_class = facultySerializer
+    serializer_class = FacultySerializer
     permission_classes = [IsAdminUser | FacultyPermission]
