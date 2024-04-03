d9cf2ab8fe32548edb67ffc40abc77c15902f6b5
bsilkyn <brent.silkyn@ugent.be>
2024-04-03 17:32:53 +0200

    chore: add basic user services #228

--- frontend/src/config/endpoints.ts
+++ frontend/src/config/endpoints.ts
@@ -35,6 +35,10 @@ export const endpoints = {
         index: '/api/admins/',
         retrieve: '/api/admins/{id}/'
     },
+    users: {
+        index: '/api/users/',
+        retrieve: '/api/users/{id}/'
+    },
     faculties: {
         index: '/api/faculties/',
         retrieve: '/api/faculties/{name}'
