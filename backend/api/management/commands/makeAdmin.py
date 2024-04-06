4b3f432de34a18aded1d1f129a6325af7d762ff0
tyboro2002 <verslypetybo@hotmail.com>
2024-04-06 22:39:53 +0200

    chore: seed assistants

---
+++ backend/api/management/commands/makeAdmin.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from api.models.student import Student
+
+class Command(BaseCommand):
+    help = 'seed the db with data'
+
+    def handle(self, *args, **options):
+        student = Student.objects.filter(username="tverslyp").get()
+        student.is_staff = True
+        student.save()
+        self.stdout.write(self.style.SUCCESS('Successfully made yourself admin!'))
--- backend/api/management/commands/seedDb.py
+++ backend/api/management/commands/seedDb.py
@@ -5,6 +5,7 @@ from faker.providers import BaseProvider
 from faker.providers import DynamicProvider
 
 from api.models.student import Student
+from api.models.assistant import Assistant
 from api.models.course import Course
 from authentication.models import Faculty
 
@@ -20,6 +21,11 @@ student_provider = DynamicProvider(
      elements=Student.objects.all(),
 )
 
+assistant_provider = DynamicProvider(
+     provider_name="assistant_provider",
+     elements=Assistant.objects.all(),
+)
+
 course_provider = DynamicProvider(
      provider_name="course_provider",
      elements=Course.objects.all(),
@@ -27,6 +33,36 @@ course_provider = DynamicProvider(
 
 # create new provider class
 class Providers(BaseProvider):
+    def provide_assistant(self, courses=None):
+        """
+        Create a assistant with the given arguments.
+        """
+        first = fake.first_name()
+        last = fake.last_name()
+        id = fake.unique.random_int(min=1, max = 9999999999999999999999)
+        faculty = [fake.faculty_provider().id for _ in range(0,fake.random_int(min=1, max = 3))] # generate 1 or 2 or 3 facultys
+        username = f"{first}_{last}_{fake.unique.random_int(min=1, max = 9999999999999999999999)}"
+        assistant = Assistant.objects.create(
+            id=id,
+            first_name=first,
+            last_name=last,
+            username=username,
+            email=username+"@example.com",
+            create_time=timezone.now(),
+            last_enrolled= timezone.now().year,
+            is_staff=fake.boolean(chance_of_getting_true=0.01)
+        )
+
+        if faculty is not None:
+            for fac in faculty:
+                assistant.faculties.add(fac)
+
+        if courses is not None:
+            for cours in courses:
+                assistant.courses.add(cours)
+
+        return assistant
+
     def provide_course(description=None, parent_course=None):
         """
         Create a Course with the given arguments.
@@ -40,7 +76,7 @@ class Providers(BaseProvider):
             parent_course=parent_course,
         )
 
-    def provide_student(self, faculty=None, courses=None):
+    def provide_student(self, courses=None):
         """
         Create a student with the given arguments.
         """
@@ -48,7 +84,7 @@ class Providers(BaseProvider):
         last = fake.last_name()
         id = fake.unique.random_int(min=1, max = 9999999999999999999999)
         faculty = [fake.faculty_provider().id for _ in range(0,fake.random_int(min=1, max = 3))] # generate 1 or 2 or 3 facultys
-        username = f"{first}_{last}_{fake.random_int(min=1, max = 9999999999999999999999)}"
+        username = f"{first}_{last}_{fake.unique.random_int(min=1, max = 9999999999999999999999)}"
         student = Student.objects.create(
             id=id,
             first_name=first,
@@ -74,6 +110,7 @@ class Providers(BaseProvider):
 def update_providers():
     faculty_provider.elements = Faculty.objects.all()
     student_provider.elements = Student.objects.all()
+    assistant_provider.elements = Assistant.objects.all()
     course_provider.elements = Course.objects.all()
 
 
@@ -82,15 +119,15 @@ fake.add_provider(Providers)
 fake.add_provider(faculty_provider)
 fake.add_provider(student_provider)
 fake.add_provider(course_provider)
+fake.add_provider(assistant_provider)
 
 class Command(BaseCommand):
     help = 'seed the db with data'
 
     def handle(self, *args, **options):
-        # Your command logic here
-        # Create a fake student instance
 
         amountOfStudents = 1
+        amountOfAssistants = 1
         amountOfCourses = 1
 
         for _ in range(0,amountOfStudents):
@@ -100,6 +137,11 @@ class Command(BaseCommand):
 
         for _ in range(0,amountOfCourses):
             fake.provide_course()
+
+        update_providers()
+
+        for _ in range(0,amountOfAssistants):
+            fake.provide_assistant()
 
         update_providers()
 
