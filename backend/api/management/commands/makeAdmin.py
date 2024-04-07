fb2dece663f4fbe367460b38697f7bed2cea442a
tyboro2002 <verslypetybo@hotmail.com>
2024-04-07 23:38:05 +0200

    chore: make admin script take in username

--- backend/api/management/commands/makeAdmin.py
+++ backend/api/management/commands/makeAdmin.py
@@ -4,8 +4,12 @@ from api.models.student import Student
 class Command(BaseCommand):
     help = 'seed the db with data'
 
+    def add_arguments(self, parser):
+        parser.add_argument('username', type=str, help='The username of the student user to make admin')
+
     def handle(self, *args, **options):
-        student = Student.objects.filter(username="tverslyp")
+        username = options['username']
+        student = Student.objects.filter(username=username)
         if student.count()==0:
             self.stdout.write(self.style.ERROR('User not found, first log in !'))
             return
