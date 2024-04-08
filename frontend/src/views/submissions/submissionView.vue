e6ab64ac89be67ef5bb030eea58bdef908b09376
lander <landermaes@outlook.com>
2024-04-08 12:33:15 +0200

    Frontend: feat FileUpload logic + styling

--- frontend/src/views/submissions/submissionView.vue
+++ frontend/src/views/submissions/submissionView.vue
@@ -1,26 +1,42 @@
 <script setup lang="ts">
 import BaseLayout from '@/components/layout/BaseLayout.vue';
 import Title from '@/components/layout/Title.vue';
+import Button from 'primevue/button';
 import {useI18n} from 'vue-i18n';
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import {useProject} from "@/composables/services/project.service.ts";
 import {useRoute} from "vue-router";
 import {useCourses} from "@/composables/services/courses.service.ts";
 import FileUpload from 'primevue/fileupload';
+import {PrimeIcons} from "primevue/api";
 
 const {t} = useI18n();
 const route = useRoute();
-const {project, getProjectByID } = useProject();
-const { course, getCourseByID } = useCourses();
+const {project, getProjectByID} = useProject();
+const {course, getCourseByID} = useCourses();
+
+/* State */
+const files = ref<File[]>([]);
 
 onMounted(async () => {
   await getProjectByID(route.params.projectId as string)
   await getCourseByID(route.params.courseId as string)
 });
 
-const onUpload = (event: any) => {
-  console.log(event)
+const onUpload = (callback: () => void) => {
+  // TODO call to server
+  files.value = [];
+  callback();
 }
+
+const onFileSelect = (event: any) => {
+  files.value = [...files.value, ...event.files];
+}
+
+const removeFile = (removeFileCallback: (index: number) => void, index: number) => {
+  removeFileCallback(index);
+  files.value.splice(index, 1);
+};
 
 function formatDate(deadline: Date): string {
   // changes deadline format to dd/mm.yyyy
@@ -47,8 +63,26 @@ function formatDate(deadline: Date): string {
             <h1>
               {{ t(`views.submissions.submit`) }}
             </h1>
-            <FileUpload mode="basic" :multiple="true" :chooseLabel="t(`views.submissions.chooseFile`)" @upload="onUpload">
+            <FileUpload class="justify-between" :show-cancel-button="false" :multiple="true"
+                        :chooseLabel="t(`views.submissions.chooseFile`)" @select="onFileSelect">
+              <template #header="{chooseCallback, clearCallback, files}">
+                <div class="flex justify-content-between align-items-center flex-1 gap-2">
+                  <Button @click="chooseCallback()" :icon="PrimeIcons.PLUS" :label="t(`views.submissions.chooseFile`)"></Button>
+                  <Button @click="onUpload(clearCallback)" :label="`Upload`" :icon="PrimeIcons.UPLOAD" :disabled="!files || files.length === 0"></Button>
+                </div>
 
+              </template>
+              <template #content="{files, removeFileCallback}">
+                <div v-if="files.length > 0" class="flex-column">
+                  <div v-for="(file, index) of files" :key="file.name + file.type + file.size"
+                       class="m-0 p-0 flex sm:p-2 justify-content-between align-items-center">
+                    <span class="font-semibold">{{ file.name }}</span>
+                    <Button class="w-2rem h-2rem" icon="pi pi-times" @click="removeFile(removeFileCallback, index)"
+                            outlined rounded
+                            severity="danger"/>
+                  </div>
+                </div>
+              </template>
             </FileUpload>
           </div>
         </div>
@@ -59,6 +93,7 @@ function formatDate(deadline: Date): string {
 
 <style scoped lang="scss">
 @import '@/assets/scss/theme/theme.scss';
+
 h1 {
   color: $primaryDarkColor
 }
