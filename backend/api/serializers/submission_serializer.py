c714852a25ac29acc3337de72112c862c755a5e5
tyboro2002 <verslypetybo@hotmail.com>
2024-04-08 17:24:30 +0200

    fix: make backend tests work

--- backend/api/serializers/submission_serializer.py
+++ backend/api/serializers/submission_serializer.py
@@ -59,7 +59,7 @@ class SubmissionSerializer(serializers.ModelSerializer):
 
         # Get the maximum submission number for the group's project
         max_submission_number = Submission.objects.filter(
-            group_project=project
+            group__project=project
         ).aggregate(Max('submission_number'))['submission_number__max'] or 0
 
         # Set the new submission number to the maximum value plus 1
