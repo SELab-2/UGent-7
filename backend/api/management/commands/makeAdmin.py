3eb8851d9368b5bcc27bd16cd305305bd5f8d862
tyboro2002 <verslypetybo@hotmail.com>
2024-04-08 17:18:16 +0200

    fix: fix linting in makeAdmin.py

--- backend/api/management/commands/makeAdmin.py
+++ backend/api/management/commands/makeAdmin.py
@@ -4,13 +4,14 @@ from api.models.student import Student
 class Command(BaseCommand):
     help = 'seed the db with data'
 
+
     def add_arguments(self, parser):
         parser.add_argument('username', type=str, help='The username of the student user to make admin')
 
     def handle(self, *args, **options):
         username = options['username']
         student = Student.objects.filter(username=username)
-        if student.count()==0:
+        if student.count() == 0:
             self.stdout.write(self.style.ERROR('User not found, first log in !'))
             return
         student = student.get()
