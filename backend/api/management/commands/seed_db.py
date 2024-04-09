ebecd8a6e234bcf3c4e6a27bbd31ff8745e001ad
tyboro2002 <verslypetybo@hotmail.com>
2024-04-09 17:49:45 +0200

    chore: commit changes

--- backend/api/management/commands/seed_db.py
+++ backend/api/management/commands/seed_db.py
@@ -491,25 +491,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         start_time = time.time()
         # TODO maybey take as option
-        amount_of_students = 50_000
-        amount_of_assistants = 300
-        amount_of_teachers = 500
-        amount_of_courses = 1_000
-        amount_of_projects = 3_000
-        amount_of_groups = 9_000
-        amount_of_submissions = 50_000
-        amount_of_file_extensions = 20
-        amount_of_structure_checks = 12_000
+        # amount_of_students = 50_000
+        # amount_of_assistants = 300
+        # amount_of_teachers = 500
+        # amount_of_courses = 1_000
+        # amount_of_projects = 3_000
+        # amount_of_groups = 9_000
+        # amount_of_submissions = 50_000
+        # amount_of_file_extensions = 20
+        # amount_of_structure_checks = 12_000
 
-        # amount_of_students = 0
-        # amount_of_assistants = 0
-        # amount_of_teachers = 0
-        # amount_of_courses = 1
-        # amount_of_projects = 0
-        # amount_of_groups = 0
-        # amount_of_submissions = 0
-        # amount_of_file_extensions = 0
-        # amount_of_structure_checks = 0
+        amount_of_students = 0
+        amount_of_assistants = 0
+        amount_of_teachers = 0
+        amount_of_courses = 0
+        amount_of_projects = 30_000
+        amount_of_groups = 0
+        amount_of_submissions = 0
+        amount_of_file_extensions = 0
+        amount_of_structure_checks = 0
 
         self.seed_data(amount_of_students, fake.provide_student, update_Student_providers)
         self.stdout.write(self.style.SUCCESS('Successfully seeded students!'))
