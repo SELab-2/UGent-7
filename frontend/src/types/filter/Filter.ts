5caf521c4e9a9ebaf7407fef8d061b4c21b28a20
EwoutV <ewoutverlinde@hotmail.com>
2024-04-09 10:23:05 +0200

    feat: better calendar view

--- frontend/src/types/filter/Filter.ts
+++ frontend/src/types/filter/Filter.ts
@@ -1,10 +1,49 @@
-export const COURSE_FILTER = {
-    search: '',
-    faculties: [],
-    years: [],
-};
+import { LocationQuery } from 'vue-router';
+
+export type CourseFilter = {
+    faculties: string[];
+    years: string[];
+} & Filter;
 
 export interface Filter {
     search: string;
-    [key: string]: any;
+    [key: string]: string|string[];
+}
+
+/**
+ * Get the course filters from the query.
+ *
+ * @param query
+ */
+export function getCourseFilters(query: LocationQuery): CourseFilter {
+    const filters: CourseFilter = {
+        search: query.search?.toString() || '',
+        faculties: [],
+        years: [],
+    };
+
+    if (query.faculties) {
+        filters.faculties = getQueryList(query.faculties as string|string[]);
+    }
+
+    if (query.years) {
+        filters.years = getQueryList(query.years as string|string[]);
+    }
+
+    return filters;
+}
+
+/**
+ * Get the query list.
+ *
+ * @param query
+ */
+function getQueryList(query: string | string[] | undefined): string[] {
+    if (query === undefined) return [];
+
+    if (Array.isArray(query)) {
+        return query;
+    }
+
+    return [query?.toString()];
 }
