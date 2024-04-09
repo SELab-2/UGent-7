fad0aaa9cd38ec42af3a3f3cca78c8f38d2a13af
Ewout Verlinde <ewoutverlinde@hotmail.com>
2024-04-09 10:34:40 +0200

    Calendar view improvements (#257)
    * chore: add make teacher script
    
    * fix: added psychology faculty icon
    
    ---------
    
    Co-authored-by: tyboro2002 <verslypetybo@hotmail.com>

---
+++ backend/api/management/commands/makeTeacher.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from api.models.student import Student
+from api.models.teacher import Teacher
+
+
+class Command(BaseCommand):
+
+    help = 'make yourself teacher'
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', type=str, help='The username of the student user to make teacher')
+
+    def handle(self, *args, **options):
+        username = options['username']
+        student = Student.objects.filter(username=username)
+        if student.count() == 0:
+            self.stdout.write(self.style.ERROR('User not found, first log in !'))
+            return
+        student = student.get()
+        Teacher.objects.create(id=student.id, create_time=student.create_time)
+        self.stdout.write(self.style.SUCCESS('Successfully made the user teacher!'))
