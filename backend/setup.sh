28f878e5fca71c40a6950eac24a627a02dfabea3
Vincent Vallaeys <vincent@vallaeys.com>
2024-04-11 21:29:50 +0100

    Run submission checks (#268)
    * chore: celery #206
    
    * chore: Support partial update for project
    
    * chore: support patch for project 2
    
    * refactor!: rework checks and submissions
    
    * chore: allow models passed to tasks
    
    * feat: added -k to test.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,6 +1,7 @@
 echo "Installing dependencies..."
 pip install poetry > /dev/null 2>&1
 poetry install > /dev/null
+
 echo "Migrating database..."
 python manage.py migrate > /dev/null
 python manage.py migrate django_celery_results > /dev/null
