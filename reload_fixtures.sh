ba65cc976a7e91330adc08d07d8bb115e64f5007
tyboro2002 <verslypetybo@hotmail.com>
2024-04-13 23:07:40 +0200

    chore: added create fixture script

--- reload_fixtures.sh
+++ reload_fixtures.sh
@@ -1,77 +1,40 @@
 #!/bin/bash
+docker-compose -f development.yml up -d backend 
 
-backend=true
-frontend=true
-build=false
-
-while getopts ":c" opt; do
-  case ${opt} in
-    c )
-      build=true
-      ;;
-    \? )
-      echo "Usage: $0 [-c]"
-      exit 1
-      ;;
-  esac
-done
-
-echo "Checking environment file..."
-if [ "$build" = true ]; then
-    rm .env > /dev/null 2>&1
-fi
-
-if ! [ -f .env ]; then
-    cp .dev.env .env
-    sed -i "s/^DJANGO_SECRET_KEY=.*/DJANGO_SECRET_KEY=totally_random_key_string/" .env
-    echo "Created environment file"
-fi
-
-echo "Checking for existing SSL certificates..."
-
-if [ ! -f "data/nginx/ssl/private.key" ] || [ ! -f "data/nginx/ssl/certificate.crt" ]; then
-    echo "Generating SSL certificates..."
-    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout data/nginx/ssl/private.key \
-    -out data/nginx/ssl/certificate.crt \
-    -subj "/C=BE/ST=/L=/O=/OU=/CN=" > /dev/null
-    echo "SSL certificates generated."
-else
-    echo "SSL certificates already exist, skipping generation."
-fi
-
-if [ "$build" = true ]; then
-    echo "Building Docker images..."
-    echo "This can take a while..."
-    docker-compose -f development.yml build --no-cache
-fi
-
-echo "Starting services..."
-docker-compose -f development.yml up -d
+# migrate database
+docker exec backend sh -c "python manage.py migrate"
 
 # seed small data
 docker exec backend sh -c "python manage.py seed_db_new small"
 docker exec backend sh -c "python manage.py seed_db small"
 
 # create fixtures for small data
-python manage.py dumpdata api --output=api/fixtures/small/small.json
-python manage.py dumpdata authentication --output=authentication/fixtures/small/small.json
-python manage.py dumpdata notifications --output=notifications/fixtures/small/small.json
+docker exec backend sh -c "python manage.py dumpdata api --output=api/fixtures/small/small.json"
+docker exec backend sh -c "python manage.py dumpdata authentication --output=authentication/fixtures/small/small.json"
+docker exec backend sh -c "python manage.py dumpdata notifications --output=notifications/fixtures/small/small.json"
 
+# seed medium data
+docker exec backend sh -c "python manage.py seed_db_new medium"
+docker exec backend sh -c "python manage.py seed_db medium"
 
-# docker exec backend sh -c "python manage.py seed_db_new medium"
-# docker exec backend sh -c "python manage.py seed_db medium"
+# create fixtures for medium data
+docker exec backend sh -c "python manage.py dumpdata api --output=api/fixtures/medium/medium.json"
+docker exec backend sh -c "python manage.py dumpdata authentication --output=authentication/fixtures/medium/medium.json"
+docker exec backend sh -c "python manage.py dumpdata notifications --output=notifications/fixtures/medium/medium.json"
 
-# docker exec backend sh -c "python manage.py seed_db_new large"
-# docker exec backend sh -c "python manage.py seed_db large"
+# seed large data
+docker exec backend sh -c "python manage.py seed_db_new large"
+docker exec backend sh -c "python manage.py seed_db large"
+
+# create fixtures for large data
+docker exec backend sh -c "python manage.py dumpdata api --output=api/fixtures/large/large.json"
+docker exec backend sh -c "python manage.py dumpdata authentication --output=authentication/fixtures/large/large.json"
+docker exec backend sh -c "python manage.py dumpdata notifications --output=notifications/fixtures/large/large.json"
 
 
 echo "-----------------"
-
 echo "Cleaning up..."
-
-docker-compose -f development.yml down
-
+docker-compose -f development.yml down backend
 echo "Done."
 
 exit $exit_code
