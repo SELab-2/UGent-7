e50698ccb1e6dd1a492218e25e9c90563279b9b5
Topvennie <vincent@vallaeys.com>
2024-04-15 15:59:06 +0200

    chore: template run checks

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -1,7 +1,8 @@
 from api.models.checks import ExtraCheck
 from api.models.student import Student
 from api.models.submission import Submission
-from api.tasks.extra_checks import task_extra_check_start
+from api.tasks.extra_check import task_extra_check_start
+from api.tasks.structure_check import task_structure_check_start
 from authentication.models import User
 from authentication.signals import user_created
 from django.db.models.signals import post_delete, post_save
@@ -9,6 +10,7 @@ from django.dispatch import Signal, receiver
 
 # Signals
 
+run_structure_checks = Signal()
 run_extra_checks = Signal()
 
 
@@ -21,6 +23,12 @@ def _user_creation(user: User, attributes: dict, **_):
 
     if student_id is not None:
         Student.create(user, student_id=student_id)
+
+
+@receiver(run_structure_checks)
+def _run_structure_checks(submission: Submission, **kwargs):
+    task_structure_check_start.apply_async((submission,))
+    return True
 
 
 @receiver(run_extra_checks)
@@ -42,6 +50,10 @@ def hook_extra_check(sender, instance: ExtraCheck, **kwargs):
                 submission.save()
 
 
-@receiver(post_save, sender=Submission)
+# TODO: Hook structure_check
+
+
+@ receiver(post_save, sender=Submission)
 def hook_submission(sender, instance: Submission, **kwargs):
+    run_structure_checks.send(sender=Submission, submission=instance)
     run_extra_checks.send(sender=Submission, submission=instance)
--- backend/api/tasks/structure_check.py
+++ backend/api/tasks/structure_check.py
@@ -2,6 +2,6 @@ from api.models.submission import Submission
 from celery import shared_task
 
 
-@shared_task
-def task_extra_check_start(submission_id):
+@shared_task()
+def task_structure_check_start(submission: Submission):
     pass
