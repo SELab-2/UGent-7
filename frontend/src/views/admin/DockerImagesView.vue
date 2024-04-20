c74171d914de7ac49efcc0df65930cea5a01e3fd
bsilkyn <brent.silkyn@ugent.be>
2024-04-20 22:15:58 +0200

    chore: update translations and add public option docker image add admin panel #334

--- frontend/src/views/admin/DockerImagesView.vue
+++ frontend/src/views/admin/DockerImagesView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import FileUpload, { type FileUploadUploaderEvent } from 'primevue/fileupload';
 import InputText from 'primevue/inputtext';
+import InputSwitch from 'primevue/inputswitch';
+import SelectButton from 'primevue/selectbutton';
 import AdminLayout from '@/components/layout/admin/AdminLayout.vue';
 import Title from '@/components/layout/Title.vue';
 import Body from '@/components/layout/Body.vue';
@@ -14,6 +16,10 @@ const { t } = useI18n();
 
 const response = ref<Response | null>(null);
 const name = ref('');
+const public_status = ref(false);
+
+const selectOptions = ref(['admin.list', 'admin.add'])
+const selected = ref<string>(t(selectOptions.value[0]));
 
 const upload = async (event: FileUploadUploaderEvent): Promise<void> => {
     const files: File[] = event.files as File[];
@@ -21,6 +27,7 @@ const upload = async (event: FileUploadUploaderEvent): Promise<void> => {
     const data = {
         file: files[0],
         name: name.value,
+        public: public_status.value
     };
     await create<Response>(endpoint, data, response, Response.fromJSON, 'multipart/form-data');
     name.value = '';
@@ -33,18 +40,30 @@ const upload = async (event: FileUploadUploaderEvent): Promise<void> => {
             <div class="gap-3 mb-3">{{ t('admin.docker_images.title') }}</div>
         </Title>
         <Body>
-            <InputText class="mb-3 gap-3" v-model:model-value="name" :placeholder="t('admin.docker_images.name')" />
-            <FileUpload
-                v-if="name.length > 0"
-                class="mb-3 gap-3"
-                :custom-upload="true"
-                @uploader="upload"
-                :file-limit="1"
-            >
-                <template #empty>
-                    <strong>No file selected.</strong>
-                </template>
-            </FileUpload>
+            <SelectButton class="mb-3 gap-3" v-model="selected" :options="selectOptions.map(t)"/>
+            <div v-if="selected === t(selectOptions[0])">
+
+            </div>
+            <div v-else>
+                <InputText class="mb-3 gap-3" v-model:model-value="name" :placeholder="t('admin.docker_images.name')" />
+                <div v-if="name.length > 0">
+                    <div class="flex align-items-center mb-3 gap-3">
+                        <label class="font-semibold w-12rem">{{ t('admin.docker_images.public') }}</label>
+                        <InputSwitch v-model:model-value="public_status" />
+                    </div>
+                    <FileUpload
+                        v-if="name.length > 0"
+                        class="mb-3 gap-3"
+                        :custom-upload="true"
+                        @uploader="upload"
+                        :file-limit="1"
+                    >
+                        <template #empty>
+                            <strong>No file selected.</strong>
+                        </template>
+                    </FileUpload>
+                </div>
+            </div>
         </Body>
     </AdminLayout>
 </template>
