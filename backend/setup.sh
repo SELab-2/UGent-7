ae3cc41b4d57b5b4766eecb4dea1e6c94968b999
Topvennie <vincent@vallaeys.com>
2024-04-27 12:09:51 +0200

    chore: I hate fixtures

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,7 +1,7 @@
 echo "Migrating database..."
-python manage.py migrate > /dev/null
+python manage.py migrate > /dev/null 2>&1
 echo "Compiling translations..."
-django-admin compilemessages > /dev/null
+django-admin compilemessages > /dev/null 2>&1
 echo "Generating Swagger documentation..."
-echo "yes" | python manage.py collectstatic > /dev/null
+echo "yes" | python manage.py collectstatic > /dev/null 2>&1
 echo "Setup complete"
