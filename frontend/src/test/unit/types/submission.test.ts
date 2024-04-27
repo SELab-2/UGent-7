020006de670bb4a30b65be3088e24c68c440e6e1
Francis Vauterin <159532420+francisvaut@users.noreply.github.com>
2024-04-27 16:19:30 +0200

    Frontend tests fix (#375)
    * test: extraCheckResult type
    
    * test: structureCheckResult type
    
    * test: submission type
    
    * test: fix submission JSON test
    
    * fix: linting errors

--- frontend/src/test/unit/types/submission.test.ts
+++ frontend/src/test/unit/types/submission.test.ts
@@ -1,8 +1,5 @@
-import { describe, it } from 'vitest';
-describe('submissions', (): void => {
-    it('gets submissions data by id', async () => {});
-});
-/*
+import { describe, it, expect } from 'vitest';
+
 import { Submission } from '@/types/submission/Submission.ts';
 import { submissionData } from './data';
 import { createSubmission } from './helper';
@@ -13,21 +10,33 @@ describe('submission type', () => {
 
         expect(submission).toBeInstanceOf(Submission);
         expect(submission.id).toBe(submissionData.id);
-        expect(submission.submission_number).toBe(submissionData.submission_number);
+        expect(submission.submission_number).toStrictEqual(submissionData.submission_number);
         expect(submission.submission_time).toStrictEqual(submissionData.submission_time);
         expect(submission.files).toStrictEqual(submissionData.files);
+        expect(submission.extraCheckResults).toStrictEqual(submissionData.extra_check_results);
+        expect(submission.structureCheckResults).toStrictEqual(submissionData.structure_check_results);
+        expect(submission.is_valid).toBe(submissionData.is_valid);
     });
 
     it('create a submission instance from JSON data', () => {
-        const submissionJSON = { ...submissionData };
-        const submission = Submission.fromJSONCreate(submissionJSON);
+        const responseSubmissionJSON = {
+            id: 'submission1_id',
+            submission_number: 1,
+            submission_time: new Date('November 1, 2024 04:20:00'),
+            files: [],
+            results: [],
+            is_valid: true,
+        };
+
+        const submission = Submission.fromJSON(responseSubmissionJSON);
 
         expect(submission).toBeInstanceOf(Submission);
         expect(submission.id).toBe(submissionData.id);
-        expect(submission.submission_number).toBe(submissionData.submission_number);
+        expect(submission.submission_number).toStrictEqual(submissionData.submission_number);
         expect(submission.submission_time).toStrictEqual(submissionData.submission_time);
         expect(submission.files).toStrictEqual(submissionData.files);
+        expect(submission.extraCheckResults).toStrictEqual([]);
+        expect(submission.structureCheckResults).toStrictEqual([]);
+        expect(submission.is_valid).toBe(submissionData.is_valid);
     });
-
 });
- */
