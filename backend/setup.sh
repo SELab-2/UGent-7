27530e3f1fa2f142298e0fe50f5109a1057ab4ed
Topvennie <vincent@vallaeys.com>
2024-04-27 12:09:51 +0200

    chore: I hate fixtures

--- backend/setup.sh
+++ backend/setup.sh
@@ -3,9 +3,5 @@ python manage.py migrate > /dev/null 2>&1
 echo "Compiling translations..."
 django-admin compilemessages > /dev/null 2>&1
 echo "Generating Swagger documentation..."
-<<<<<<< HEAD
 echo "yes" | python manage.py collectstatic > /dev/null 2>&1
-=======
-echo "yes" | python manage.py collectstatic > /dev/null
->>>>>>> 50c8072 (chore: networking)
 echo "Setup complete"
