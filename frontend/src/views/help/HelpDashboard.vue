110741cfc4932236ecef580ddcc50fc05a20774a
tyboro2002 <verslypetybo@hotmail.com>
2024-04-29 08:05:08 +0200

    chore: startup sidebar menu

--- frontend/src/views/help/HelpDashboard.vue
+++ frontend/src/views/help/HelpDashboard.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import HelpBaseLayout from '@/components/help/HelpBaseLayout.vue';
+import Button from 'primevue/button';
+import { RouterLink } from 'vue-router';
+//TODO add menu
 
+const items = [
+  { label: 'Home', route: 'help-student' },
+  { label: 'About', route: 'help-teacher' },
+  // Add more sidebar items as needed
+];
 </script>
 
 <template>
     <HelpBaseLayout>
-
+        <div>
+            
+        </div>
     </HelpBaseLayout>
 </template>
 
