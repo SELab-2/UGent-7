a78367117d0d1a75e212d841c9150acb493c8545
Topvennie <vincent@vallaeys.com>
2024-05-05 21:51:02 +0200

    chore: remove submission routes

--- backend/api/views/submission_view.py
+++ backend/api/views/submission_view.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.mixins import RetrieveModelMixin
 
 from ..models.submission import Submission
 from ..serializers.submission_serializer import SubmissionSerializer
 
 
 # TODO: Permission to ask for logs
-class SubmissionViewSet(viewsets.ModelViewSet):
+class SubmissionViewSet(RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Submission.objects.all()
     serializer_class = SubmissionSerializer
