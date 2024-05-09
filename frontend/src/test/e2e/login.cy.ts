9a56f45322ed5d2ff6ca58e6438006c8d4ca4ad8
bsilkyn <brent.silkyn@ugent.be>
2024-05-09 19:20:45 +0200

    test: add some more loging and logout related tests #241

--- frontend/src/test/e2e/login.cy.ts
+++ frontend/src/test/e2e/login.cy.ts
@@ -13,10 +13,22 @@ describe('login', () => {
     // })
     it('does not redirect to login page when logged in', () => {
         // log in as a test student
-        cy.visit('/api/auth/test-user/student/')
+        cy.visit('/api/auth/test-user/student/');
         // visit dashboard
-        cy.visit('/')
+        cy.visit('/');
         // should not be redirected to login page
         cy.url().should('not.match', /^.*\/auth\/login$/);
+        // logout for cleaning up
+        cy.get('#logout').click();
+    });
+    it('redirects to login page after logging out', () => {
+       // log in as a test student
+        cy.visit('/api/auth/test-user/student/');
+        // visit dashboard
+        cy.visit('/');
+        // logout for cleaning up
+        cy.get('#logout').click();
+        // should be on login page now
+        cy.url().should('match', /^.*\/auth\/login$/);
     });
 });
