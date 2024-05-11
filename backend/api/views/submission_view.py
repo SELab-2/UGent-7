2d4572334d960c4ebf869aee7ca41d6d2df379f7
Topvennie <vincent@vallaeys.com>
2024-05-11 16:48:32 +0200

    chore: Remove submission destroy capabilities

--- backend/api/views/submission_view.py
+++ backend/api/views/submission_view.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
-from rest_framework.mixins import DestroyModelMixin, RetrieveModelMixin
+from rest_framework.mixins import RetrieveModelMixin
 
 from ..models.submission import Submission
 from ..serializers.submission_serializer import SubmissionSerializer
 
 
 # TODO: Permission to ask for logs
-class SubmissionViewSet(RetrieveModelMixin, viewsets.GenericViewSet, DestroyModelMixin):
+class SubmissionViewSet(RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Submission.objects.all()
     serializer_class = SubmissionSerializer
