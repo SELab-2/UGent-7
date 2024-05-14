968bf1b38c1111573f36491dfd100a330a408be0
Topvennie <vincent@vallaeys.com>
2024-05-14 11:55:32 +0200

    fix: cypress tests

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'http://nginx',
+        baseUrl: 'https://nginx',
         specPattern: 'src/test/e2e/**/*.cy.{js,jsx,ts,tsx}',
     },
 });
