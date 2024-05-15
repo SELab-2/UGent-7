8a4f1e32615aef698e749fbbba3ded74481ed406
Topvennie <vincent@vallaeys.com>
2024-05-15 19:50:38 +0200

    chore: docker images permissions

--- backend/api/serializers/docker_serializer.py
+++ backend/api/serializers/docker_serializer.py
@@ -13,12 +13,21 @@ class DockerImageSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         data = super().validate(attrs=attrs)
 
-        if not self.partial:
-            data["owner"] = self.context["request"].user
-        elif "owner" not in data:
-            data["owner"] = self.instance.owner
+        if "owner" in data and data["owner"] != self.context["request"].user and not self.context["request"].user.is_staff:
+            # Only allow staff to set the owner of an image to someone else
+            raise ValidationError(_("docker.errors.no_staff"))
+
+        if "owner" not in data:
+            # Add the owner data if not present
+            if not self.partial:
+                # If it's created assign the user who made the request
+                data["owner"] = self.context["request"].user
+            else:
+                # Else use the pre exisiting owner
+                data["owner"] = self.instance.owner
 
         if "public" in data and data["public"] and not data["owner"].is_staff:
+            # Only allow staff to have public images
             raise ValidationError(_("docker.errors.custom"))
 
         return data
