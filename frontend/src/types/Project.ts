f9353d61054e025138984b3ab58095c668a5f6a8
Topvennie <vincent@vallaeys.com>
2024-05-16 14:40:04 +0200

    Revert "Merge remote-tracking branch 'origin/main' into development"
    This reverts commit 2024d88b950ddbc78ca40e1316b0cb6d8333e01a, reversing
    changes made to 863b69203b2eb33379fc1eecc09b00e6e7aada98.

--- frontend/src/types/Project.ts
+++ frontend/src/types/Project.ts
@@ -4,6 +4,7 @@ import { type ExtraCheck } from './ExtraCheck.ts';
 import { type Group } from './Group.ts';
 import { type StructureCheck } from './StructureCheck.ts';
 import { type Submission } from './submission/Submission.ts';
+import { SubmissionStatus } from '@/types/SubmisionStatus.ts';
 
 export class Project {
     constructor(
@@ -18,7 +19,8 @@ export class Project {
         public max_score: number,
         public score_visible: boolean,
         public group_size: number,
-        public course: Course | null = null,
+        public course: Course,
+        public status: SubmissionStatus,
         public structure_file: File | null = null,
         public structureChecks: StructureCheck[] | null = null,
         public extra_checks: ExtraCheck[] | null = null,
@@ -91,7 +93,12 @@ export class Project {
      * @returns True if the project is locked, false otherwise.
      */
     public isLocked(): boolean {
-        return !this.visible || this.archived || this.locked_groups || moment(this.start_date).isAfter();
+        return (
+            !this.visible ||
+            this.archived ||
+            this.locked_groups ||
+            moment(this.start_date).isBefore(moment().startOf('day'))
+        );
     }
 
     /**
@@ -100,12 +107,6 @@ export class Project {
      * @param project
      */
     static fromJSON(project: Project): Project {
-        let course = null;
-
-        if (project.course !== null && project.course !== undefined) {
-            course = Course.fromJSON(project.course);
-        }
-
         return new Project(
             project.id,
             project.name,
@@ -118,7 +119,8 @@ export class Project {
             project.max_score,
             project.score_visible,
             project.group_size,
-            course,
+            Course.fromJSON(project.course),
+            SubmissionStatus.fromJSON(project.status),
         );
     }
 }
