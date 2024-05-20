4af559db20f88ed4b3f952ea3939b752acbb0a52
francis <francis.vauterin@ugent.be>
2024-05-20 11:49:01 +0200

    chore: file upload component

--- frontend/src/components/projects/TestSubmission.vue
+++ frontend/src/components/projects/TestSubmission.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import Button from 'primevue/button';
 import { PrimeIcons } from 'primevue/api';
+import { onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { onMounted } from 'vue';
 import { useSubmission } from '@/composables/services/submission.service.ts';
 import { type Project } from '@/types/Project.ts';
+import FileUpload from 'primevue/fileupload';
+
+/* State */
+const files = ref<File[]>([]);
+
+const group = {value: {id: "5"}}
 
 /* Props */
 const props = defineProps<{
@@ -13,9 +19,29 @@ const props = defineProps<{
 
 /* Composable injections */
 const { t } = useI18n();
-const { submissions, getSubmissionByGroup } = useSubmission();
+const { submission, submissions, createSubmission, getSubmissionByGroup } = useSubmission();
 
 /* Functions */
+const onFileSelect = (event: any): void => {
+    files.value = [...files.value, ...event.files];
+};
+
+const removeFile = (removeFileCallback: (index: number) => void, index: number): void => {
+    removeFileCallback(index);
+    files.value.splice(index, 1);
+};
+
+const onUpload = async (callback: () => void): Promise<void> => {
+    if (group.value !== null) {
+        await createSubmission(files.value as File[], group.value.id);
+        if (submission.value != null) {
+            submissions.value = [...(submissions.value ?? []), submission.value];
+        }
+        files.value = [];
+        callback();
+    }
+};
+
 const testSubmission = async (): Promise<void> => {
     const fileInput = document.createElement('input');
     fileInput.type = 'file';
@@ -31,9 +57,48 @@ const testSubmission = async (): Promise<void> => {
 </script>
 
 <template>
-    <Button @click="testSubmission">
-        Upload a test submission
-    </Button>
+    <FileUpload
+        class="justify-between"
+        :show-cancel-button="false"
+        :multiple="true"
+        :chooseLabel="t(`views.submissions.chooseFile`)"
+        @select="onFileSelect"
+    >
+        <template #header="{ chooseCallback, clearCallback, files }">
+            <div class="flex justify-content-between align-items-center flex-1 gap-2">
+                <Button
+                    @click="chooseCallback()"
+                    :icon="PrimeIcons.PLUS"
+                    :label="t(`views.submissions.chooseFile`)"
+                ></Button>
+                <Button
+                    @click="onUpload(clearCallback)"
+                    :label="`Upload`"
+                    :icon="PrimeIcons.UPLOAD"
+                    :disabled="!files || files.length === 0"
+                ></Button>
+            </div>
+        </template>
+        <template #content="{ files, removeFileCallback }">
+            <div v-if="files.length > 0" class="flex-column">
+                <div
+                    v-for="(file, index) of files"
+                    :key="file.name + file.type + file.size"
+                    class="m-0 p-0 flex sm:p-2 justify-content-between align-items-center"
+                >
+                    <span class="font-semibold">{{ file.name }}</span>
+                    <Button
+                        class="w-2rem h-2rem"
+                        icon="pi pi-times"
+                        @click="removeFile(removeFileCallback, index)"
+                        outlined
+                        rounded
+                        severity="danger"
+                    />
+                </div>
+            </div>
+        </template>
+    </FileUpload>
 </template>
 
 <style scoped lang="scss"></style>
