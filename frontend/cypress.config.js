7de35934c0416cc1cd5768f2d9bec0b8ba79ff72
bsilkyn <brent.silkyn@ugent.be>
2024-05-21 12:05:50 +0200

    chore: trying to fix adding data before tests #434

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'https://nginx',
+        baseUrl: 'https://localhost',
         specPattern: 'src/test/e2e/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'src/test/e2e/setup/e2e.ts',
     },
 });
---
+++ frontend/src/test/e2e/setup/data.ts
@@ -0,0 +1,15 @@
+const date = new Date();
+
+export const faculty = {
+    id: '0',
+    name: 'Faculty'
+};
+
+export const course = {
+    name: 'Course',
+    academic_startyear: date.getMonth() > 8 ? date.getFullYear() : date.getFullYear() - 1,
+    excerpt: 'This is a course',
+    faculty: faculty,
+    private_course: false
+};
+
