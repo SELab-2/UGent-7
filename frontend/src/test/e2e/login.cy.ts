4f7642c0e2cffcb43d47d8141950883ceb758320
bsilkyn <brent.silkyn@ugent.be>
2024-05-22 14:11:49 +0200

    test: fix test #407

--- frontend/src/test/e2e/login.cy.ts
+++ frontend/src/test/e2e/login.cy.ts
@@ -16,14 +16,18 @@ describe('login', () => {
         cy.get('#logout').click();
     });
     it('redirects to login page after logging out', () => {
+        // intercept the retrieval of student info
+        cy.intercept('/api/students/*/').as('student')
         // log in as a test student
         cy.visit('/api/auth/test-user/student/');
         // visit dashboard
         cy.visit('/');
+        // wait for requests for student info to end
+        cy.wait('@student');
+        cy.wait('@student');
+        cy.wait('@student');
         // logout
         cy.get('#logout').click();
-        // try to go to dashboard
-        cy.visit('/');
         // should be on login page now
         cy.url().should('match', /^.*\/auth\/login$/);
     });
