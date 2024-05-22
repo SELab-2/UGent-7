472c3ce5f6fa1fa14e565ce04b9dcfe80d3eee0f
bsilkyn <brent.silkyn@ugent.be>
2024-05-22 15:53:26 +0200

    test: automatically login all users, before every test file, so that they're available, but also automatically logout after every test #434

---
+++ frontend/cypress/support/e2e.ts
@@ -0,0 +1,65 @@
+// ***********************************************************
+// This example support/e2e.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands.ts'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // log uncaught error
+  console.error('Uncaught exception:', err.message);
+
+  return !err.message.includes('401');
+});
+
+const logout = () => {
+    cy.getCookie('csrftoken').then((cookie) => {
+        cy.getCookie('sessionid').then((cookie2) => {
+            if (cookie && cookie2) {
+                cy.request({
+                    method: 'POST',
+                    url: '/api/auth/cas/logout/',
+                    headers: {
+                        'Referer': Cypress.config('baseUrl'),
+                        'X-CSRFToken': cookie.value,
+                    },
+                });
+            }
+        })
+
+    });
+}
+
+before(() => {
+    cy.intercept('POST', '/api/auth/cas/logout/', (req) => {
+        console.log('intercepted');
+        req.headers['Referer'] = Cypress.config('baseUrl');
+
+        return req;
+    });
+
+    cy.request('POST', '/api/auth/test-user/student/');
+    logout();
+    cy.request('POST', '/api/auth/test-user/multi/');
+    logout();
+    cy.request('POST', '/api/auth/test-user/admin/');
+    logout();
+})
+
+afterEach(() => {
+    logout();
+})
--- frontend/src/test/e2e/login.cy.ts
+++ frontend/src/test/e2e/login.cy.ts
@@ -16,16 +16,10 @@ describe('login', () => {
         cy.get('#logout').click();
     });
     it('redirects to login page after logging out', () => {
-        // intercept the retrieval of student info
-        cy.intercept('/api/students/*/').as('student');
         // log in as a test student
         cy.request('POST', '/api/auth/test-user/student/');
         // visit dashboard
         cy.visit('/');
-        // wait for requests for student info to end
-        cy.wait('@student');
-        cy.wait('@student');
-        cy.wait('@student');
         // logout
         cy.get('#logout').click();
         // should be on login page now
